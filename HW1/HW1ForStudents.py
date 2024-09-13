e77929a2c848bce72b42db67d8d791c91e6fd3c2
fabioski01 <fabioski0101@gmail.com>
2024-09-13 22:06:35 +0200

    fix(input in mpa)

--- HW1/HW1ForStudents.py
+++ HW1/HW1ForStudents.py
@@ -5,7 +5,7 @@ import numpy as np
 L =  250 # mm 	# total length of the bar (scalar value)
 Nelem = 4 # - 	#number of elements (scalar value)
 F = 1000 # N  	# applied force on the final nodes (scalar value)
-E = 70e9 # Pa 	# elastic modulus (scalar value)
+E = 70e3 # MPa 	# elastic modulus (scalar value)
 BC = np.array([[1 ],[1 ]]) #form: [node numbers], [applied displacements]; each vector can have multiple inputs
 # example BC: np.array([[i],[x]]) should apply a displacement of x on node i
 # example 2 BC: np.array([[i, j],[x, y]]) should apply a displacement of x on node i and a displacement of y on onde j
